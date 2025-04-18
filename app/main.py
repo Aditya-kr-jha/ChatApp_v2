ce0522190ea4f2df4fa374d0455960e2778ab7c5
Aditya Jha <aditya.k@spotbusinc.com>
2025-04-18 11:56:16 +0530

    added allowed origins

--- app/main.py
+++ app/main.py
@@ -48,9 +48,15 @@ app.include_router(user_router)
 app.include_router(channel_router)
 app.include_router(messages_router)
 
+allowed_origins = [
+    "http://localhost:5173",
+    "http://127.0.0.1:5173",
+    "https://dg9qzy1dkyjl5.cloudfront.net",
+]
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # In production, specify exact origins
+    allow_origins=allowed_origins,  # In production, specify exact origins
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
